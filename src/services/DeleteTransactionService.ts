6c649fbdd5226c58c12e6fc5e624e24a1cd520f5
Wil-g2 <wil-g2@hotmail.com>
2020-04-26 22:58:25 -0300

    fix: fix bug without await in remove method in DeleteTransactionService

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -5,12 +5,12 @@ import TransactionsRepository from '../repositories/TransactionsRepository';
 class DeleteTransactionService {
   public async execute(id: string): Promise<void> {
     const transactionsRepository = getCustomRepository(TransactionsRepository);
-    const transaction = await transactionsRepository.findOne({ id });
+    const transaction = await transactionsRepository.findOne(id);
     if (!transaction) {
       throw new AppError('Transaction not found');
     }
 
-    transactionsRepository.remove(transaction);
+    await transactionsRepository.remove(transaction);
   }
 }
 
